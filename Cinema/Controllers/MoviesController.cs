cde723a29c5a004f0ae66ad46cf070b7317f4f52
andrii <sneyp67@gmail.com>
2016-11-09 01:49:05 +0200

    RegExp for routeconfig for month and year in action Released

--- Cinema/Controllers/MoviesController.cs
+++ Cinema/Controllers/MoviesController.cs
@@ -14,7 +14,36 @@ namespace Cinema.Controllers
         {
             var movie = new Movie() { Name= "Shrek"  };
 
-            return View(movie);
+
+             return View(movie);
+            // return Content("Hello World");
+            // return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index","Home",new { age=1, sortBy="name"});
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("Id="+ id);
+        }
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+
+
+
+        }
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+
+            return Content(year+ "/"+ month);
         }
     }
 }
